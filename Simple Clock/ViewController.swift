dfcc9a4964d2ab573a58e08dd5ac01e065794e5d
Michael Adams <madams6216@gmail.com>
2015-11-03 10:22:22 -0500

    created clock app

--- Simple Clock/ViewController.swift
+++ Simple Clock/ViewController.swift
@@ -13,13 +13,34 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        NSTimer.scheduledTimerWithTimeInterval(1, target: self, selector: "updateClock", userInfo: nil, repeats: true )
+
     }
+    @IBOutlet weak var Label: UILabel!
+    
+    
+    
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    func updateClock() {
+        
+        let date = NSDate()
+        
+        
+        
+        let calendar = NSCalendar.currentCalendar()
+        
+        
+        let componenents = calendar.components([NSCalendarUnit.Hour,NSCalendarUnit.Minute,NSCalendarUnit.Second], fromDate: date)
+        
+        self.Label.text = "\(String(componenents.hour)):\(String(componenents.minute)) \(String(componenents.second) )"
+        
+    }
 
-
+    
 }
 
